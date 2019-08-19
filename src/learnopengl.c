86c951b8d34c6351957ae431f457c69b8477702a
Stefan Eng <eng_steff@hotmail.com>
2019-08-19 20:48:58 +0200

    Set vertex-color through attributes.

--- src/learnopengl.c
+++ src/learnopengl.c
@@ -25,15 +25,10 @@ processingInput(GLFWwindow * window)
 }
 
 GLfloat vertices[] = {
-   0.5f,  0.5f,  0.0f, // top right.
-   0.5f, -0.5f,  0.0f, // bottom right.
-  -0.5f, -0.5f,  0.0f, // bottom left.
-  -0.5f,  0.5f,  0.0f, // top left.
-};
-
-GLuint indices[] = {
-  0, 1, 3, // First triangle.
-  1, 2, 3, // Second triangle.
+  // Positions          // Colors
+   0.5f, -0.5f,  0.0f,  1.0f,  0.0f,  0.0f, // bottom right.
+  -0.5f, -0.5f,  0.0f,  0.0f,  1.0f,  0.0f, // bottom left.
+   0.0f,  0.5f,  0.0f,  0.0f,  0.0f,  1.0f, // top.
 };
 
 int
@@ -140,23 +135,20 @@ main(void)
   glGenVertexArrays(1, &VAO);
   glBindVertexArray(VAO);
 
-  GLuint EBO = 0;
-  glGenBuffers(1, &EBO);
-  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-  glBufferData(
-    GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW
-  );
-
   GLuint VBO = 0;
   glGenBuffers(1, &VBO);
   glBindBuffer(GL_ARRAY_BUFFER, VBO);
   glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(GLfloat), 0);
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6*sizeof(GLfloat), 0);
   glEnableVertexAttribArray(0);
 
+  glVertexAttribPointer(
+      1, 3, GL_FLOAT, GL_FALSE, 6*sizeof(GLfloat), (void *)(3*sizeof(float))
+  );
+  glEnableVertexAttribArray(1);
+
   glUseProgram(program_shader);
-  //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
   int location_vertex_color = glGetUniformLocation(program_shader, "ourColor");
 
@@ -171,8 +163,7 @@ main(void)
     glUniform4f(location_vertex_color, 0.0f, greenValue, 0.0f, 1.0f);
 
     glBindVertexArray(VAO);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
 
     glfwSwapBuffers(window);
     glfwPollEvents();
