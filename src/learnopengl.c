a2e3f4cc95377553c3d0297d7aa909c3c3f7a4cb
Stefan Eng <eng_steff@hotmail.com>
2019-08-20 14:03:07 +0200

    Create texture and render box.

--- src/learnopengl.c
+++ src/learnopengl.c
@@ -18,10 +18,16 @@ processingInput(GLFWwindow * window)
 }
 
 GLfloat vertices[] = {
-  // Positions          // Colors
-   0.5f, -0.5f,  0.0f,  1.0f,  0.0f,  0.0f, // bottom right.
-  -0.5f, -0.5f,  0.0f,  0.0f,  1.0f,  0.0f, // bottom left.
-   0.0f,  0.5f,  0.0f,  0.0f,  0.0f,  1.0f, // top.
+  // Positions         // Colors         // Texture coords.
+   0.5f,  0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f, // top right.
+   0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f, // bottom right.
+  -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, // bottom left.
+  -0.5f,  0.5f, 0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f, // top left.
+};
+
+GLuint indices[] = {
+  0, 1, 2, // First triangle.
+  0, 2, 3, // Second triangle.
 };
 
 int
@@ -65,22 +71,62 @@ main(void)
   glGenVertexArrays(1, &VAO);
   glBindVertexArray(VAO);
 
+  GLuint EBO = 0;
+  glGenBuffers(1, &EBO);
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+  glBufferData(
+    GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW
+  );
+
   GLuint VBO = 0;
   glGenBuffers(1, &VBO);
   glBindBuffer(GL_ARRAY_BUFFER, VBO);
   glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6*sizeof(GLfloat), 0);
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8*sizeof(GLfloat), 0);
   glEnableVertexAttribArray(0);
 
   glVertexAttribPointer(
-      1, 3, GL_FLOAT, GL_FALSE, 6*sizeof(GLfloat), (void *)(3*sizeof(float))
+      1, 3, GL_FLOAT, GL_FALSE, 8*sizeof(GLfloat), (void *)(3*sizeof(float))
   );
   glEnableVertexAttribArray(1);
+
+  glVertexAttribPointer(
+      2, 2, GL_FLOAT, GL_FALSE, 8*sizeof(GLfloat), (void *)(6*sizeof(float))
+  );
+  glEnableVertexAttribArray(2);
 
   glUseProgram(program_shader);
 
   int location_vertex_color = glGetUniformLocation(program_shader, "ourColor");
+
+  GLuint texture = 0;
+  glGenTextures(1, &texture);
+  glBindTexture(GL_TEXTURE_2D, texture);
+
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+  int width, height, nrChannels;
+  const char * res_texture = "res/container.jpg";
+  unsigned char * data = stbi_load(
+    res_texture, &width, &height, &nrChannels, 0
+  );
+  if (data) {
+    glTexImage2D(
+      GL_TEXTURE_2D, 0, GL_RGB, width, height,
+      0, GL_RGB, GL_UNSIGNED_BYTE, data
+    );
+    glGenerateMipmap(GL_TEXTURE_2D);
+  } else {
+    fprintf(stderr, "Could not create texture from %s.\n", res_texture);
+    return -1;
+  }
+  stbi_image_free(data);
+
+  glBindTexture(GL_TEXTURE_2D, texture);
 
   while (!glfwWindowShouldClose(window)) {
     processingInput(window);
@@ -93,7 +139,7 @@ main(void)
     glUniform4f(location_vertex_color, 0.0f, greenValue, 0.0f, 1.0f);
 
     glBindVertexArray(VAO);
-    glDrawArrays(GL_TRIANGLES, 0, 3);
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
     glfwSwapBuffers(window);
     glfwPollEvents();
