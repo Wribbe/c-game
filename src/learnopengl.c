76b2d29f73cc7663fd95c56da199dbb5d309b738
Stefan Eng <eng_steff@hotmail.com>
2019-10-08 17:11:44 +0200

    Remove shift-rotation.

--- src/learnopengl.c
+++ src/learnopengl.c
@@ -22,7 +22,6 @@ GLfloat fov = 45.0f;
 int64_t flags = {0};
 
 vec3 position_light = {1.0f, 3.0f, 0.4f};
-GLfloat angle_rotation_view = 0.0f;
 
 #define NR_POINT_LIGHTS 4
 
@@ -130,18 +129,10 @@ processingInput(GLFWwindow * window)
     vec3_add(camera_position, camera_position, temp);
   }
   if (glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS) {
-    if (flags_get(SHIFT_DOWN)) {
-      angle_rotation_view += 1.0f*time_delta;
-    } else {
-      camera_reorient(-speed_camera_arrows, 0.0f);
-    }
+    camera_reorient(-speed_camera_arrows, 0.0f);
   }
   if (glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS) {
-    if (flags_get(SHIFT_DOWN)) {
-      angle_rotation_view -= 1.0f*time_delta;
-    } else {
-      camera_reorient(speed_camera_arrows, 0.0f);
-    }
+    camera_reorient(speed_camera_arrows, 0.0f);
   }
   if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS) {
     camera_reorient(0.0f, speed_camera_arrows);
@@ -522,8 +513,6 @@ main(void)
       camera_target,
       camera_up
     );
-
-    mat4x4_rotate_Z(view, view, angle_rotation_view);
 
     mat4x4_perspective(
       projection, to_rad(fov), (float)SCREEN_WIDTH/(float)SCREEN_HEIGHT,
